2faa528f4deceb217fec53bcb634eae72200e559
Mehak goyal <47946110+mehakg867@users.noreply.github.com>
2022-09-24 18:30:51 +0530

    background changes

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require ('path');
-app.use(express.static("D:/Fitness&Wellness/client/Public"));
+app.use(express.static("./Public"));
 
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html")
