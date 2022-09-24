6be67e2e87c633cc0dd07c310ae6108ddd4a4195
Mehak goyal <47946110+mehakg867@users.noreply.github.com>
2022-09-24 23:44:08 +0530

    Merge pull request #1 from shrutidy16/Shruti
    Shruti

--- client/index.js
+++ client/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require ('path');
+app.use(express.static("./Public"));
 
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html")
 });
 
+app.get("/FitnessTv",function(rq,res){
+    res.sendFile(__dirname+"/tv.html")
+});
+app.get("/MentalWellnessTv",function(rq,res){
+    res.sendFile(__dirname+"/welltv.html")
+});
+app.get("/BMIcalc",function(rq,res){
+    res.sendFile(__dirname+"/bmi.html")
+});
 app.listen(3000, function(){
 
 })
